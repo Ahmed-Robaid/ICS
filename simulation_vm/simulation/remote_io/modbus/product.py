8e1e698d9536dc544ea74570dec07b3bf418b9b4
ARAM_AHMED <robaid77@gmail.com>
2025-01-01 21:50:56 +0300

    Update product.py

--- simulation_vm/simulation/remote_io/modbus/product.py
+++ simulation_vm/simulation/remote_io/modbus/product.py
@@ -16,76 +16,57 @@ This can also be done with a python thread::
 # TODO maybe cut down on calls to simulation by combining all remote io into one file?
 import socket
 import json
+import asyncio
+
 # --------------------------------------------------------------------------- #
 # import the modbus libraries we need
 # --------------------------------------------------------------------------- #
-from pymodbus.server.async_io import StartTcpServer
+from pymodbus.server.async_io import StartAsyncTcpServer
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusServerContext, ModbusSlaveContext
-#from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
-#import random
 from pymodbus import __version__ as version
 
-# --------------------------------------------------------------------------- #
-# import the twisted libraries we need
-# --------------------------------------------------------------------------- #
-from twisted.internet.task import LoopingCall
 
-
-# --------------------------------------------------------------------------- #
-# define your callback process
-# --------------------------------------------------------------------------- #
-
-import logging
-
-# Configure logging
-logging.basicConfig()
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
 last_command = -1
-def updating_writer(a):
+async def updating_writer(context, sock):
     global last_command
-    print('updating')
-    context  = a[0]
+    while True:
+        print('updating PRODUCT')
+        slave_id = 0x01 # slave address
 
-    slave_id = 0x01 # slave address
-    count = 50
-    s = a[1]
+        current_command = context[slave_id].getValues(16, 1, 1)[0]/ 65535.0 *100.0
+        sock.sendall(('{"request":"write","data":{"inputs":{"product_valve_sp":'+repr(current_command)+'}}}\n').encode())
+        try:
+            data = json.loads(sock.recv(1500).decode())
+        except json.JSONDecodeError:
+            print("Received data is not in JSON format.")
+            await asyncio.sleep(1)
+            return
+        valve_pos = int(data["state"]["product_valve_pos"]/100.0*65535)
+        flow = int(data["outputs"]["product_flow"]/500.0*65535)
+        print(data)
 
+        valve_pos = max(0, min(valve_pos, 65535))
+        flow = max(0, min(flow, 65535))
 
-    current_command = context[slave_id].getValues(16, 1, 1)[0]/ 65535.0 *100.0
+        # import pdb; pdb.set_trace()
+        context[slave_id].setValues(4, 1, [valve_pos,flow])
 
-    s.sendall(('{"request":"write","data":{"inputs":{"product_valve_sp":' + repr(current_command) + '}}}\n').encode('utf-8'))
-    data = json.loads(s.recv(1500).decode('utf-8'))
-    valve_pos = int(data["state"]["product_valve_pos"]/100.0*65535)
-    flow = int(data["outputs"]["product_flow"]/500.0*65535)
-    print(data)
-    if valve_pos > 65535:
-        valve_pos = 65535
-    elif valve_pos < 0:
-        valve_pos = 0
-    if flow > 65535:
-        flow = 65535
-    elif flow < 0:
-        flow = 0
-    # import pdb; pdb.set_trace()
-    context[slave_id].setValues(4, 1, [valve_pos,flow])
+        await asyncio.sleep(1)
 
 
-
-def run_update_server():
+async def run_update_server():
     # ----------------------------------------------------------------------- #
     # initialize your data store
     # ----------------------------------------------------------------------- #
-
-
     store = ModbusSlaveContext(
-        di=ModbusSequentialDataBlock(0, list(range(1, 101))),
-        co=ModbusSequentialDataBlock(0, list(range(101, 201))),
-        hr=ModbusSequentialDataBlock(0, list(range(201, 301))),
-        ir=ModbusSequentialDataBlock(0, list(range(301, 401)))
+        di=ModbusSequentialDataBlock(0, range(1, 101)),
+        co=ModbusSequentialDataBlock(0, range(101, 201)),
+        hr=ModbusSequentialDataBlock(0, range(201, 301)),
+        ir=ModbusSequentialDataBlock(0, range(301, 401))
     )
+
     context = ModbusServerContext(slaves=store, single=True)
 
     # ----------------------------------------------------------------------- #
@@ -97,20 +78,25 @@ def run_update_server():
     identity.VendorUrl = 'http://github.com/bashwork/pymodbus/'
     identity.ProductName = 'pymodbus Server'
     identity.ModelName = 'pymodbus Server'
-    identity.MajorMinorRevision = version.short()
+    identity.MajorMinorRevision = version  # '1.0'
+
     # connect to simulation
     HOST = '127.0.0.1'
     PORT = 55555
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
+
+    # ----------------------------------------------------------------------- #
+    # run the updating task
+    # ----------------------------------------------------------------------- #
+    asyncio.create_task(updating_writer(context, sock))
+
     # ----------------------------------------------------------------------- #
     # run the server you want
     # ----------------------------------------------------------------------- #
-    time = 1  # 5 seconds delay
-    loop = LoopingCall(f=updating_writer, a=(context,sock))
-    loop.start(time, now=False)  # initially delay by time
-    StartTcpServer(context=context, identity=identity, address=("192.168.95.13", 502))
+    await StartAsyncTcpServer(context=context, identity=identity, address=("192.168.168.13", 502))
 
 
 if __name__ == "__main__":
-    run_update_server()
+    asyncio.run(run_update_server())
+
