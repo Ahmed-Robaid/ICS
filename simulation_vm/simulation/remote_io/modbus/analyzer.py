dcbc135bfa90464aa0408d4cf633bc677118e14d
Ahmed Rabed <robaid77@gmail.com>
2025-02-28 16:44:13 +0300

    Update analyzer.py

--- simulation_vm/simulation/remote_io/modbus/analyzer.py
+++ simulation_vm/simulation/remote_io/modbus/analyzer.py
@@ -19,10 +19,12 @@ import asyncio
 # --------------------------------------------------------------------------- #
 # import the modbus libraries we need
 # --------------------------------------------------------------------------- #
-from pymodbus.server.async_io import StartAsyncTcpServer
+from pymodbus.server import StartAsyncTcpServer
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusServerContext, ModbusSlaveContext
+#from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
+import random
 from pymodbus import __version__ as version
 
 # --------------------------------------------------------------------------- #
@@ -37,45 +39,46 @@ log.setLevel(logging.INFO)
 # define your callback process
 # --------------------------------------------------------------------------- #
 
+
 async def updating_writer(context, sock):
+    print('updating ANALYZER')
+    readfunction = 0x03 # read holding registers
+    writefunction = 0x10
+    slave_id = 0x01 # slave address
+    count = 50
+
+    sock.sendall(b'{"request":"read"}')
+    #data = s.recv(1500).decode('utf-8')
+    data = json.loads(sock.recv(1500).decode())
+
+    print(data)
+
+    a_in_purge = int(data["outputs"]["A_in_purge"] * 65535)
+    b_in_purge = int(data["outputs"]["B_in_purge"] * 65535)
+    c_in_purge = int(data["outputs"]["C_in_purge"] * 65535)
+
+    # import pdb; pdb.set_trace()
+    context[slave_id].setValues(4, 1, [a_in_purge, b_in_purge, c_in_purge])
+    values = context[slave_id].getValues(readfunction, 0, 2)
+    log.debug("Values from datastore: " + str(values))
+
+async def run_updating_task(context, sock):
     while True:
-        print('updating ANALYZER')
-        readfunction = 0x03 # read holding registers
-        writefunction = 0x10
-        slave_id = 0x01 # slave address
-        # import pdb; pdb.set_trace()
-        sock.send(b'{"request":"read"}')
-        try:
-            data = json.loads(sock.recv(1500).decode())
-        except json.JSONDecodeError:
-            print("Received data is not in JSON format.")
-            await asyncio.sleep(1)
-            return
-        a_in_purge = int(data["outputs"]["A_in_purge"]*65535)
-        b_in_purge = int(data["outputs"]["B_in_purge"]*65535)
-        c_in_purge = int(data["outputs"]["C_in_purge"]*65535)
-        print(data)
-
-        # import pdb; pdb.set_trace()
-        context[slave_id].setValues(4, 1, [a_in_purge,b_in_purge,c_in_purge])
-        values = context[slave_id].getValues(readfunction, 0, 2)
-        log.debug("Values from datastore: " + str(values))
-
-
-        await asyncio.sleep(1)
+        await updating_writer(context, sock)
+        await asyncio.sleep(1)  # 1-second delay
 
 
 async def run_update_server():
     # ----------------------------------------------------------------------- #
     # initialize your data store
     # ----------------------------------------------------------------------- #
+
+
     store = ModbusSlaveContext(
         di=ModbusSequentialDataBlock(0, range(1, 101)),
         co=ModbusSequentialDataBlock(0, range(101, 201)),
         hr=ModbusSequentialDataBlock(0, range(201, 301)),
-        ir=ModbusSequentialDataBlock(0, range(301, 401))
-    )
-
+        ir=ModbusSequentialDataBlock(0, range(301, 401)))
     context = ModbusServerContext(slaves=store, single=True)
 
     # ----------------------------------------------------------------------- #
@@ -87,7 +90,7 @@ async def run_update_server():
     identity.VendorUrl = 'http://github.com/bashwork/pymodbus/'
     identity.ProductName = 'pymodbus Server'
     identity.ModelName = 'pymodbus Server'
-    identity.MajorMinorRevision = version  # '1.0'
+    identity.MajorMinorRevision = version
 
     # connect to simulation
     HOST = '127.0.0.1'
@@ -95,15 +98,12 @@ async def run_update_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
 
-    # ----------------------------------------------------------------------- #
-    # run the updating task
-    # ----------------------------------------------------------------------- #
-    asyncio.create_task(updating_writer(context, sock))
+    asyncio.create_task(run_updating_task(context, sock))
 
     # ----------------------------------------------------------------------- #
-    # run the server you want
+    # run the server
     # ----------------------------------------------------------------------- #
-    await StartAsyncTcpServer(context=context, identity=identity, address=("192.168.168.15", 502))
+    await StartAsyncTcpServer(context, identity=identity, address=("192.168.95.15", 502))
 
 
 if __name__ == "__main__":
