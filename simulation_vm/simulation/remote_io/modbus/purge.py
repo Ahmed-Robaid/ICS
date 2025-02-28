e7d5931cb10517c7075fbad2745daff22358e1b8
Ahmed Rabed <robaid77@gmail.com>
2025-02-28 16:41:20 +0300

    Update purge.py

--- simulation_vm/simulation/remote_io/modbus/purge.py
+++ simulation_vm/simulation/remote_io/modbus/purge.py
@@ -20,52 +20,53 @@ import asyncio
 # --------------------------------------------------------------------------- #
 # import the modbus libraries we need
 # --------------------------------------------------------------------------- #
-from pymodbus.server.async_io import StartAsyncTcpServer
+from pymodbus.server import StartAsyncTcpServer
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusServerContext, ModbusSlaveContext
+#from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
+import random
 from pymodbus import __version__ as version
 
-# --------------------------------------------------------------------------- #
 
 last_command = -1
 async def updating_writer(context, sock):
     global last_command
+    print('updating PURGE')
+
+    slave_id = 0x01 # slave address
+    count = 50
+
+    current_command = context[slave_id].getValues(16, 1, 1)[0] / 65535.0 *100.0
+
+    sock.sendall(('{"request":"write","data":{"inputs":{"purge_valve_sp":' + repr(current_command) + '}}}\n').encode())
+
+    data = json.loads(sock.recv(1500).decode())
+    valve_pos = int(data["state"]["purge_valve_pos"]/100.0*65535)
+    flow = int(data["outputs"]["purge_flow"]/500.0*65535)
+    print(data)
+    valve_pos = max(0, min(valve_pos, 65535))  # Clamp value between 0 and 65535
+    flow = max(0, min(flow, 65535))  # Clamp value between 0 and 65535
+
+    context[slave_id].setValues(4, 1, [valve_pos,flow])
+
+async def run_updating_task(context, sock):
     while True:
-        print('updating PURGE')
-        slave_id = 0x01 # slave address
-
-        current_command = context[slave_id].getValues(16, 1, 1)[0] / 65535.0 *100.0
-        sock.sendall(('{"request":"write","data":{"inputs":{"purge_valve_sp":'+repr(current_command)+'}}}\n').encode())
-        try:
-            data = json.loads(sock.recv(1500).decode())
-        except json.JSONDecodeError:
-            print("Received data is not in JSON format.")
-            await asyncio.sleep(1)
-            return
-        valve_pos = int(data["state"]["purge_valve_pos"]/100.0*65535)
-        flow = int(data["outputs"]["purge_flow"]/500.0*65535)
-        print(data)
-
-        valve_pos = max(0, min(valve_pos, 65535))
-        flow = max(0, min(flow, 65535))
-
-        # import pdb; pdb.set_trace()
-        context[slave_id].setValues(4, 1, [valve_pos,flow])
-        await asyncio.sleep(1)
+        await updating_writer(context, sock)
+        await asyncio.sleep(1)  # 1-second delay
 
 
 async def run_update_server():
     # ----------------------------------------------------------------------- #
     # initialize your data store
     # ----------------------------------------------------------------------- #
+
+
     store = ModbusSlaveContext(
         di=ModbusSequentialDataBlock(0, range(1, 101)),
         co=ModbusSequentialDataBlock(0, range(101, 201)),
         hr=ModbusSequentialDataBlock(0, range(201, 301)),
-        ir=ModbusSequentialDataBlock(0, range(301, 401))
-    )
-
+        ir=ModbusSequentialDataBlock(0, range(301, 401)))
     context = ModbusServerContext(slaves=store, single=True)
 
     # ----------------------------------------------------------------------- #
@@ -77,7 +78,7 @@ async def run_update_server():
     identity.VendorUrl = 'http://github.com/bashwork/pymodbus/'
     identity.ProductName = 'pymodbus Server'
     identity.ModelName = 'pymodbus Server'
-    identity.MajorMinorRevision = version  # '1.0'
+    identity.MajorMinorRevision = version
 
     # connect to simulation
     HOST = '127.0.0.1'
@@ -85,15 +86,12 @@ async def run_update_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
 
-    # ----------------------------------------------------------------------- #
-    # run the updating task
-    # ----------------------------------------------------------------------- #
-    asyncio.create_task(updating_writer(context, sock))
+    asyncio.create_task(run_updating_task(context, sock))
 
     # ----------------------------------------------------------------------- #
-    # run the server you want
+    # run the server
     # ----------------------------------------------------------------------- #
-    await StartAsyncTcpServer(context=context, identity=identity, address=("192.168.168.12", 502))
+    await StartAsyncTcpServer(context, identity=identity, address=("192.168.95.12", 502))
 
 
 if __name__ == "__main__":
