8813ebfde123eb023b68a06481f6334dc80ae094
Ahmed Rabed <robaid77@gmail.com>
2025-02-28 16:38:55 +0300

    Update feed1.py

--- simulation_vm/simulation/remote_io/modbus/feed1.py
+++ simulation_vm/simulation/remote_io/modbus/feed1.py
@@ -6,70 +6,79 @@ context in an SQLite4 database while the server is operating.
 
 This scrit generates a random address range (within 0 - 65000) and a random
 value and stores it in a database. It then reads the same address to verify
-that the process works as expected
+that the processworks as expected
 
 This can also be done with a python thread::
     from threading import Thread
     thread = Thread(target=updating_writer, args=(context,))
     thread.start()
 """
+# TODO maybe cut down on calls to simulation by combining all remote io into one file?
 import socket
 import json
 import asyncio
 # --------------------------------------------------------------------------- #
 # import the modbus libraries we need
 # --------------------------------------------------------------------------- #
-from pymodbus.server.async_io import StartAsyncTcpServer
+from pymodbus.server import StartAsyncTcpServer
 from pymodbus.device import ModbusDeviceIdentification
 from pymodbus.datastore import ModbusSequentialDataBlock
 from pymodbus.datastore import ModbusServerContext, ModbusSlaveContext
+#from pymodbus.transaction import ModbusRtuFramer, ModbusAsciiFramer
+import random
 from pymodbus import __version__ as version
 
+
+
 # --------------------------------------------------------------------------- #
+# define your callback process
+# --------------------------------------------------------------------------- #
+# import logging
+# logging.basicConfig()
+# log = logging.getLogger()
+# log.setLevel(logging.INFO)
 
 last_command = -1
-
 async def updating_writer(context, sock):
     global last_command
+    print('updating FEED 1')
+
+    slave_id = 0x01 # slave address
+    count = 50
+
+    #Write multiple analog output registers(0x10)
+
+    current_command = context[slave_id].getValues(16, 1, 1)[0] / 65535.0 *100.0
+    sock.sendall(('{"request":"write","data":{"inputs":{"f1_valve_sp":' + repr(current_command) + '}}}\n').encode())
+
+    data = json.loads(sock.recv(1500).decode())
+    valve_pos = int(data["state"]["f1_valve_pos"]/100.0*65535)
+    flow = int(data["outputs"]["f1_flow"]/500.0*65535)
+    print(data)
+
+    valve_pos = max(0, min(valve_pos, 65535))  # Clamp value between 0 and 65535
+    flow = max(0, min(flow, 65535))  # Clamp value between 0 and 65535
+
+    context[slave_id].setValues(4, 1, [valve_pos,flow])
+
+
+async def run_updating_task(context, sock):
     while True:
-        print('updating FEED 1')
-        slave_id = 0x01 # slave address
-        count = 50
-
-        current_command = context[slave_id].getValues(16, 1, 1)[0] / 65535.0*100.0
-        sock.sendall(('{"request":"write","data":{"inputs":{"f1_valve_sp":' + repr(current_command) + '}}}\n').encode())
-        # import pdb; pdb.set_trace()
-        try:
-            data = json.loads(sock.recv(1500).decode())
-        except json.JSONDecodeError:
-            print("Received data is not in JSON format.")
-            await asyncio.sleep(1)
-            return
-        valve_pos = int(data["state"]["f1_valve_pos"]/100.0*65535)
-        flow = int(data["outputs"]["f1_flow"]/500.0*65535)
-        print(data)
-
-        valve_pos = max(0, min(valve_pos, 65535))
-        flow = max(0, min(flow, 65535))
-
-        # import pdb; pdb.set_trace()
-        context[slave_id].setValues(4, 1, [valve_pos,flow])
-
-
-        await asyncio.sleep(1)
-
+        await updating_writer(context, sock)
+        await asyncio.sleep(1)  # 1-second delay
 
 async def run_update_server():
     # ----------------------------------------------------------------------- #
     # initialize your data store
     # ----------------------------------------------------------------------- #
+
+
     store = ModbusSlaveContext(
         di=ModbusSequentialDataBlock(0, range(1, 101)),
         co=ModbusSequentialDataBlock(0, range(101, 201)),
         hr=ModbusSequentialDataBlock(0, range(201, 301)),
         ir=ModbusSequentialDataBlock(0, range(301, 401))
     )
-
     context = ModbusServerContext(slaves=store, single=True)
 
     # ----------------------------------------------------------------------- #
@@ -81,7 +90,7 @@ async def run_update_server():
     identity.VendorUrl = 'http://github.com/bashwork/pymodbus/'
     identity.ProductName = 'pymodbus Server'
     identity.ModelName = 'pymodbus Server'
-    identity.MajorMinorRevision = version  # '1.0'
+    identity.MajorMinorRevision = version #'1.0'
 
     # connect to simulation
     HOST = '127.0.0.1'
@@ -89,17 +98,13 @@ async def run_update_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((HOST, PORT))
 
-    # ----------------------------------------------------------------------- #
-    # run the updating task
-    # ----------------------------------------------------------------------- #
-    asyncio.create_task(updating_writer(context, sock))
+    asyncio.create_task(run_updating_task(context, sock))
 
     # ----------------------------------------------------------------------- #
-    # run the server you want
+    # run the server
     # ----------------------------------------------------------------------- #
-    await StartAsyncTcpServer(context=context, identity=identity, address=("192.168.168.10", 502))
+    await StartAsyncTcpServer(context, identity=identity, address=("192.168.95.10", 502))
 
 
 if __name__ == "__main__":
     asyncio.run(run_update_server())
-
